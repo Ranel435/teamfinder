4812284535717b99254a18dc6a42b59dd9a145c0
Ranel <67388819+Ranel435@users.noreply.github.com>
2024-12-07 18:06:07 +0300

    Update main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -42,10 +42,10 @@ func main() {
 
 	// CORS configuration
 	router.GetRouter().Use(cors.New(cors.Config{
-		// AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8090"},
+		// AllowOrigins:     []string{"http://localhost:3000"},
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Accept", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,

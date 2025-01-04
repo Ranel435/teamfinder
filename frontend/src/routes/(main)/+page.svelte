c366417c7d4573f8b83109e1460cdd3a0cfcf693
qquerellka <uskovmaksof@gmail.com>
2025-01-04 23:20:10 +0300

    fronted main_page codestyle

--- frontend/src/routes/(main)/+page.svelte
+++ frontend/src/routes/(main)/+page.svelte
@@ -1,6 +1,6 @@
-<script lang="ts">
+ <script lang="ts">
     import "../../app.css";
-    import {type Form} from "$lib/stores/data"
+    import {type Form} from "$lib/stores/data";
 
     let roles: string[] = ["Backend", "Frontend", "Fullstack", "Analyst", "Product-Manager", "Design", "IOS-Developer", "ML", "DevOps"]
     let array: number[] = [1, 2, 3, 4, 5, 6]
@@ -23,60 +23,58 @@
 
 <main>
     <section class="main">
-        <div class="text">
-            <p>Команда найдется для каждого</p>
+        <div class="main__content">
+            <p class="main__text">Команда найдется для каждого</p>
+            <img class="main__image" src="Начни Хакатонить!.svg" alt="Начни хакатонить!">
         </div>
-        <div class="main-text">
-            <img src="Начни Хакатонить!.svg" alt="">
+        <div class="main__buttons">
+            <a class="main__button" href="/forms">Смотреть анкеты</a>
+            <a class="main__button" href="/teams">Найти команду</a>
+            <a class="main__button" href="/create_form">Создать анкету</a>
         </div>
-        <div class="main-buttons">
-            <a  class="main-button" href="/forms">Смотреть анкеты</a>
-            <a class="main-button" href="">Найти команду</a>
-            <a class="main-button" href="">Создать анкету</a>
-        </div> 
     </section>
     <section class="popular-roles">
-        <h2 class="title">Популярные роли</h2>
-        <div class="roles-grid">
+        <h2 class="popular-roles__title">Популярные роли</h2>
+        <div class="popular-roles__roles">
             {#each roles as role, index}
-            <button class="role {role}">
-                <div class="role-name">{role}</div>
-                <div class="role-teams">{count_teams} команд</div>
-              </button>
+            <button class="popular-roles__role role {role}">
+                <p class="role__name">{role}</p>
+                <p class="role__teams">{count_teams} команд</p>
+            </button>
             {/each}
         </div>
-        <div class="more-roles">
-            <a class="more" href="/">Еще роли</a>
+        <div class="more">
+            <a class="more__link" href="/">Еще роли</a>
         </div>
     </section>
 
-    <section class="teams_slots">
-        <h2 class="teams_slots-title">Поиск сокомандников</h2>
-        <div class="slots-grid">
+    <section class="teams">
+        <h2 class="teams__title">Поиск сокомандников</h2>
+        <div class="teams__slots">
             {#each array as item}
-            <div class="slots-grid-slot">
-                <h4>Ищем</h4>
-                <h3 class="slot-role">Роль</h3>
-                <p class="slot-skills">Стек/навыки · Опыт</p>
+            <div class="teams__slot slot">
+                <h3 class="slot__title">Ищем</h3>
+                <h4 class="slot__role">Роль</h4>
+                <p class="slot__skills">Стек/навыки · Опыт</p>
                 <hr>
-                <h3 class="slot-team_name">Название команды</h3>
-                <div class="slot-players">
-                    <p class="slot-players-player">Игрок 1</p>
-                    <p class="slot-players-player">Игрок 2</p>
-                    <p class="slot-players-player">Игрок 3</p>
-                    <p class="slot-players-player">Игрок 4</p>
+                <h3 class="slot__team-name gradient-text">Название команды</h3>
+                <div class="slot__players">
+                    <p class="slot__player">Игрок 1</p>
+                    <p class="slot__player">Игрок 2</p>
+                    <p class="slot__player">Игрок 3</p>
+                    <p class="slot__player">Игрок 4</p>
                 </div>
-                <div class="slot-features">
-                    <p>1 хак</p>
-                    <p>0 побед</p>
-                    <p>3м 5д в сборе</p>
+                <div class="slot__features">
+                    <p class="slot__feature">1 хак</p>
+                    <p class="slot__feature">0 побед</p>
+                    <p class="slot__feature">3м 5д в сборе</p>
                 </div>
-                <button class="slot-more_about">Подробнее</button>
+                <button class="slot__more-about">Подробнее</button>
             </div>
             {/each}
         </div>
-        <div class="more-roles">
-            <a class="more" href="/">Другие команды</a>
+        <div class="more">
+            <a class="more__link" href="/">Другие команды</a>
         </div>
     </section>
 </main>
@@ -84,7 +82,7 @@
 
 
 <style>
-    * {
+    p, h2, h4 {
         color: #fff;
     }
 
@@ -92,28 +90,13 @@
         width: 100%;
     }
 
-    img {
-        width: 815px;
-        height: 258px;
-    }
-
-    p {
-        font-family: "Igra";
-        font-size: 18px;
-        line-height: 130%;
-        letter-spacing: 5%;
-        color: #fff;
-    }
-
     main {
-        /* margin: 256px 0; */
-        /* background-image: url(".static/PinkEllipse.svg"), url("/static/YellowEllipse.svg"), url("/static/Star.svg"); 
-        background-repeat: no-repeat, no-repeat, no-repeat; 
-        background-position: center center, center center, center center; 
-        background-size: cover; */
         margin: 100px 80px;
     }
 
+
+/* /////////////////////////////////////////////////////////////////////     */
+    
     .main {
         display: flex;
         justify-content: center;
@@ -121,24 +104,30 @@
         flex-direction: column;
     }
 
-    .main-text {
+    .main__content {
         text-align: center;
         margin: 10px 0 40px;
     }
 
-    .main-buttons {
+    .main__text {
+        font-family: "Igra";
+        color: #fff;
+    }
+
+    .main__image {
+        width: 815px;
+        height: 258px;
+    }
+
+    .main__buttons {
         margin-bottom: 128px;
     }
-    
-    
 
-    /* main buttons */
-    .main-button {
+    .main__button {
         font-family: 'Manrope';
         color: #fff;
         text-decoration: none;
 
-        max-width: 164px;
         border: 3px solid;
         border-color: #fff;
         border-radius: 50px;
@@ -148,11 +137,13 @@
         padding: 5px 16px;
     }
 
-    .main-button:hover {
+    .main__button:hover {
         transition: all 0.7s ease;
         color: #000;
         background-color: #fff;
     }
+
+/* ////////////////////////////////////////////////////////////////////////// */
 
     .popular-roles {
         display: flex;
@@ -164,7 +155,7 @@
         margin-top: 0;
     }
 
-    .title, .teams_slots-title {
+    .popular-roles__title, .teams__title {
         font-family: "Igra";
         font-weight: normal;
         color: #fff;
@@ -172,35 +163,36 @@
         font-size: 56px;
     }
 
-    .roles-grid {
+    .popular-roles__roles {
         display: grid;
         grid-template-columns: repeat(3, minmax(auto, 100vw));
     }
 
-    .role {
+    .popular-roles__role {
         position: relative; 
         overflow: hidden; 
         display: flex;
-        flex: 1;
         flex-direction: column;
         justify-content: center;
         align-items: flex-start;
         text-align: left;
+
         border: 4px solid #fff;
         border-radius: 24px;
         padding: 10px;
         margin: 0px 36px 36px 0;
+
         background: rgba(0, 0, 0,0);
         cursor: pointer;
         font-family: 'Manrope';   
-        color: #fff;  
         transition: transform 0.2s, border-color 0.2s;
     }
-    .role:nth-child(3n) {
+
+    .popular-roles__role:nth-child(3n) {
         margin-right: 0;
     }
 
-    .role::before {
+    .popular-roles__role::before {
         content: ""; 
         position: absolute;
         top: 0;
@@ -212,24 +204,23 @@
         transition: width 0.3s ease;
     }
 
-    .role:hover::before {
-        width: 100%; /* Заполняем всю кнопку */
+    .popular-roles__role:hover::before {
+        width: 100%;
     }
 
-    .role-name {
-        font-weight: medium;
+    .role__name {
         margin-bottom: 10px;
         padding-left: 19px;
         font-size: 27px;
     }
 
-    .role-teams {
+    .role__teams {
         padding-left: 19px;
         padding-bottom: 10px;
         font-size: 18px;
     }
 
-    .more-roles {
+    .more {
         display: flex;
         justify-content: right;
         margin-top: 20px;
@@ -238,61 +229,43 @@
         width: 100%;
     }
 
-    .more {
+    .more__link {
         font-family: "Igra";
         font-size: 24px;
         color: #fff;
         text-decoration: none;
     }
 
-    .more:hover {
+    .more__link:hover {
         text-decoration: underline;
     }
 
-    .Backend::before {
-        background-color: rgba(0, 31, 184, 0.3);
-    }
-    .Frontend::before {
-        background-color: rgba(255, 77, 0, 0.3);
-    }
-    .Fullstack::before {
-        background-color: rgba(255, 39, 39, 0.3);
-    }
-    .Analyst::before {
-        background-color: rgba(0, 197, 3, 0.3);
-    }
-    .ML::before {
-        background-color: rgba(0, 90, 12, 0.3);
-    }
-    .DevOps::before {
-        background-color: rgba(153, 0, 255, 0.3);
-    }
-    .Design::before {
-        background-color: rgba(14, 179, 255, 0.3);
-    }
-    .IOS-Developer::before {
-        background-color: rgba(160, 0, 83, 0.3);
-    }
-    .Product-Manager::before {
-        background-color: rgba(255, 225, 0, 0.3);
-    }
+    .Backend::before {background-color: rgba(0, 31, 184, 0.3);}
+    .Frontend::before {background-color: rgba(255, 77, 0, 0.3);}
+    .Fullstack::before {background-color: rgba(255, 39, 39, 0.3);}
+    .Analyst::before {background-color: rgba(0, 197, 3, 0.3);}
+    .ML::before {background-color: rgba(0, 90, 12, 0.3);}
+    .DevOps::before {background-color: rgba(153, 0, 255, 0.3);}
+    .Design::before {background-color: rgba(14, 179, 255, 0.3);}
+    .IOS-Developer::before {background-color: rgba(160, 0, 83, 0.3);}
+    .Product-Manager::before {background-color: rgba(255, 225, 0, 0.3);}
 
-    .teams_slots {
+/* /////////////////////////////////////////////////////////////////////// */
+
+    .teams {
         display: flex;
         flex-direction: column;
         align-items: center;
-
         margin-top: 128px;
     }
 
-    .slots-grid {
+    .teams__slots {
         display: grid;
         grid-template-columns: repeat(3, minmax(auto, 100vw));
     }
 
-    .slots-grid-slot {
+    .teams__slot {
         display: flex;
-        flex: 1;
         flex-direction: column;
         justify-content: center;
         align-items: flex-start;
@@ -303,53 +276,44 @@
         padding: 16px;
         margin: 0px 36px 36px 0;
         font-family: 'Manrope';
-        color: #fff;  
     }
 
-    .slots-grid-slot:nth-child(3n) {
+    .teams__slot:nth-child(3n) {
         margin-right: 0;
     }
 
-
-    .slots-grid-slot h4 {
+    .slot__title {
         color: var(--pink);
         font-size: 12px;
         font-weight: 500;
     }
 
-    .slot-role {
+    .slot__role {
         font-size: 24px;
     }
 
-    .slot-skills {
+    .slot__skills {
         font-size: 12px;
         color: var(--dark-grey);
     }
 
-    .slot-team_name {
+    .slot__team-name {
         font-size: 16px;
         font-weight: 500;
-        color: var(--pink);
-        background-image: var(--gradient); /* Укажите градиент */
-        -webkit-background-clip: text; /* Обрезка фона по тексту (для WebKit-браузеров) */
-        background-clip: text; /* Обрезка фона по тексту (для других браузеров) */
-        color: transparent; /* Делаем цвет текста прозрачным */
     }
 
-    .slot-players {
+    .slot__players {
         display: grid;
         grid-template-columns: repeat(2, 1fr);
         margin: 8px 0 8px;
     }
 
-    .slot-players-player {
-        font-family: 'Manrope';
-        font-weight: 300;
+    .slot__player {
         font-size: 14px;
         margin: 0px 8px 8px 0;
     }
 
-    .slot-features {
+    .slot__features {
         display: flex;
         flex-direction: row;
         justify-content: left;
@@ -358,16 +322,17 @@
         margin-bottom: 21px;
     }
 
-    .slot-features p {
+    .slot__feature {
         font-size: 10px;
-        background-color: #9C004A;
+        background-color: var(--burgundy);
         padding: 5px 13px;
         border-radius: 12px;
         margin-right: 10px;
     }
 
-    .slot-more_about {
+    .slot__more-about {
         font-size: 14px;
+        color: #fff;
         padding: 8px 0;
         width: 100%;
         border: 2px solid;
@@ -377,9 +342,10 @@
         cursor: pointer;
     }
 
-    .slot-more_about:hover {
+    .slot__more-about:hover {
         transition: all 0.7s ease;
         color: #000;
         background-color: #fff;
     }
+
 </style>
